58567daa223cd2ea460dc7a34030ec1fb7fee15f
Peter Reinhardt <peter@segment.io>
2013-07-31 14:05:41 -0700

    updating testing code without app state hooks

---
+++ Tests/TestPodApp/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Analytics
+#define COCOAPODS_POD_AVAILABLE_Analytics
+#define COCOAPODS_VERSION_MAJOR_Analytics 0
+#define COCOAPODS_VERSION_MINOR_Analytics 5
+#define COCOAPODS_VERSION_PATCH_Analytics 3
+
