75ee6d88fc6c53f2885e70d8bd18a15845aa6e39
Tony Xiao <tony@collections.me>
2013-08-18 00:21:47 -0700

    Added TestAppIOS target, misc bug fixes in Analytics.m

---
+++ Analytics/TestAppIOS/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  TestAppIOS
+//
+//  Created by Tony Xiao on 8/17/13.
+//  Copyright (c) 2013 Segment.io. All rights reserved.
+//
+
+
+#import "Analytics.h"
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [Analytics withSecret:@"k5l6rrye0hsv566zwuk7"];
+    [[Analytics sharedAnalytics] debug:YES];
+    [[Analytics sharedAnalytics] reset];
+}
+
+- (IBAction)identify:(id)sender {
+    NSLog(@"test identify");
+    [[Analytics sharedAnalytics] identify:@"Khan"];
+}
+
+- (IBAction)track:(id)sender {
+    NSLog(@"test track");
+    [[Analytics sharedAnalytics] track:@"Hello Event"];
+}
+
+- (IBAction)screen:(id)sender {
+    NSLog(@"test screen");
+    [[Analytics sharedAnalytics] screen:@"Initial Screen"];
+}
+
+@end
