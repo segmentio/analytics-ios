749963cd0dab39314b32289a5f77b3d7d70f6f6d
Tony Xiao <tony@collections.me>
2013-08-23 02:56:06 -0700

    Adding CocoaPodsExample project

---
+++ Examples/CocoaPodsExample/CocoaPodsExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CocoaPodsExample
+//
+//  Created by Tony Xiao on 8/23/13.
+//  Copyright (c) 2013 Segment.io. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
