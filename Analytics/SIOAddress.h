de74440e680c90d45568a37c3b460df6fe2f1d22
Travis Jeffery <tj@travisjeffery.com>
2014-04-25 11:37:39 -0500

    Add SIOAddress helper (#127)

---
+++ Analytics/SIOAddress.h
@@ -0,0 +1,23 @@
+//
+//  SIOLocation.h
+//  Analytics
+//
+//  Created by Travis Jeffery on 4/25/14.
+//  Copyright (c) 2014 Segment.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SIOAddress : NSObject
+
+@property (nonatomic, copy, readonly) NSString *city;
+@property (nonatomic, copy, readonly) NSString *country;
+@property (nonatomic, copy, readonly) NSString *postalCode;
+@property (nonatomic, copy, readonly) NSString *state;
+@property (nonatomic, copy, readonly) NSString *street;
+@property (nonatomic, strong, readonly) NSNumber *longitude;
+@property (nonatomic, strong, readonly) NSNumber *latitude;
+
+@property (nonatomic, assign, readonly) BOOL hasKnownLocation;
+
+@end
