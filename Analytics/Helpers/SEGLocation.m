64176907ea26b0a4872dafcf284b847fa52d7606
Travis Jeffery <tj@travisjeffery.com>
2014-06-03 01:55:47 -0400

    Add logging for CLLocationManager failing

--- Analytics/Helpers/SEGLocation.m
+++ Analytics/Helpers/SEGLocation.m
@@ -7,6 +7,7 @@
 //
 
 #import "SEGLocation.h"
+#import <SEGAnalyticsUtils.h>
 
 #import <CoreLocation/CoreLocation.h>
 
@@ -20,73 +21,76 @@
 
 @implementation SEGLocation
 
-- (id)init {    
-    if (![CLLocationManager locationServicesEnabled]) return nil;
-
-    if (self = [super init]) {
-        _geocoder = [[CLGeocoder alloc] init];
-        _locationManager = [[CLLocationManager alloc] init];
-        _locationManager.delegate = self;
-        _locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
-        [_locationManager startUpdatingLocation];
-    }
-    return self;
+- (id)init {
+  if (![CLLocationManager locationServicesEnabled]) return nil;
+  
+  if (self = [super init]) {
+    self.geocoder = [[CLGeocoder alloc] init];
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager startUpdatingLocation];
+  }
+  return self;
 }
 
 - (NSString *)state {
-    return self.currentPlacemark.administrativeArea;
+  return self.currentPlacemark.administrativeArea;
 }
 
 - (NSString *)country {
-    return self.currentPlacemark.country;
+  return self.currentPlacemark.country;
 }
 
 - (NSString *)city {
-    return self.currentPlacemark.locality;
+  return self.currentPlacemark.locality;
 }
 
 - (NSString *)postalCode {
-    return self.currentPlacemark.postalCode;
+  return self.currentPlacemark.postalCode;
 }
 
 - (NSString *)street {
-    return self.currentPlacemark.thoroughfare;
+  return self.currentPlacemark.thoroughfare;
 }
 
 - (BOOL)hasKnownLocation {
-    return self.currentPlacemark != nil;
+  return self.currentPlacemark != nil;
 }
 
 - (NSNumber *)latitude {
-    return @(self.currentPlacemark.location.coordinate.latitude);
+  return @(self.currentPlacemark.location.coordinate.latitude);
 }
 
 - (NSNumber *)longitude {
-    return @(self.currentPlacemark.location.coordinate.longitude);
+  return @(self.currentPlacemark.location.coordinate.longitude);
 }
 
 - (NSNumber *)speed {
-    return @(self.currentPlacemark.location.speed);
+  return @(self.currentPlacemark.location.speed);
 }
 
 - (NSDictionary *)locationDictionary {
-    return [self dictionaryWithValuesForKeys:@[ @"city", @"country", @"latitude", @"longitude", @"speed" ]];
+  return [self dictionaryWithValuesForKeys:@[ @"city", @"country", @"latitude", @"longitude", @"speed" ]];
 }
 
 - (NSDictionary *)addressDictionary {
-    return [self dictionaryWithValuesForKeys:@[ @"city", @"country", @"postalCode", @"state", @"street" ]];
+  return [self dictionaryWithValuesForKeys:@[ @"city", @"country", @"postalCode", @"state", @"street" ]];
 }
 
 #pragma mark - CLLocationManagerDelegate
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    if (!locations.count) return;
+  if (!locations.count) return;
+  
+  __weak typeof(self) weakSelf = self;
+  [self.geocoder reverseGeocodeLocation:locations.firstObject completionHandler:^(NSArray *placemarks, NSError *error) {
+    __strong typeof(weakSelf) strongSelf = weakSelf;
+    strongSelf.currentPlacemark = placemarks.firstObject;
+  }];
+}
 
-    __weak typeof(self) weakSelf = self;
-    [self.geocoder reverseGeocodeLocation:locations.firstObject completionHandler:^(NSArray *placemarks, NSError *error) {
-        __strong typeof(weakSelf) strongSelf = weakSelf;
-        strongSelf.currentPlacemark = placemarks.firstObject;
-    }];
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+  SEGLog(@"error: %@", error);
 }
 
 @end
