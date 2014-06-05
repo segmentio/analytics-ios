0714ee2f2d99a36c4f3a426d4bf05864004f5cc0
Travis Jeffery <tj@travisjeffery.com>
2014-06-05 01:04:32 -0500

    Add taplytics

---
+++ Analytics/Integrations/Taplytics/SEGTaplytics.m
@@ -0,0 +1,50 @@
+//
+//  SEGTaplytics.m
+//  Analytics
+//
+//  Created by Travis Jeffery on 6/4/14.
+//  Copyright (c) 2014 Segment.io. All rights reserved.
+//
+
+#import "SEGTaplytics.h"
+#import "SEGAnalyticsUtils.h"
+
+#import <Taplytics/Taplytics.h>
+
+@implementation SEGTaplytics
+
++ (void)load {
+  [SEGAnalytics registerIntegration:self withIdentifier:[self identifier]];
+}
+
+- (id)init {
+  if (self = [super init]) {
+    self.name = [self.class identifier];
+    self.valid = NO;
+    self.initialized = NO;
+  }
+  return self;
+}
+
+- (void)start {
+  [Taplytics startTaplyticsAPIKey:[self apiKey]];
+  
+  SEGLog(@"TapstreamIntegration initialized with api key %@", [self apiKey]);
+}
+
+- (void)validate {
+  self.valid = ([self apiKey] != nil);
+}
+
+#pragma mark - Private
+
+- (NSString *)apiKey {
+  return self.settings[@"apiKey"];
+}
+
+
++ (NSString *)identifier {
+  return @"Taplytics";
+}
+
+@end
