333d5a581038dcbd82d8cb49d177c930f5c26ba6
Travis Jeffery <tj@travisjeffery.com>
2014-06-16 15:40:21 -0500

    1.3.0

--- Analytics/Analytics.h
+++ Analytics/Analytics.h
@@ -1,5 +1,5 @@
 // Analytics.h
 // Copyright (c) 2014 Segment.io. All rights reserved.
-// Version 1.2.1 (Do not change this line. It is automatically modified by the build process)
+// Version 1.3.0 (Do not change this line. It is automatically modified by the build process)
 
 #import "SEGAnalytics.h"
