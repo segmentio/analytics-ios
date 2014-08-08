062332446801929207891884e3fe70bb04d3186f
Travis Jeffery <tj@travisjeffery.com>
2014-08-07 20:52:08 -0500

    Fix building local pods

--- scripts/build.rb
+++ scripts/build.rb
@@ -42,10 +42,10 @@ module Build
       @pods ||= begin
                   amplitude = { :name => 'Amplitude', :dependencies => [{ :name => 'Amplitude-iOS', :version => '~> 2.1.0' }]}
                   bugsnag = { :name => 'Bugsnag', :dependencies => [{ :name => 'Bugsnag', :version => '~> 3.1.2' }]}
-                  countly = { :name => 'Countly', :dependencies => [{ :name => 'Countly', :version => '~> 2.0.0' }]}
+                  countly = { :name => 'Countly', :dependencies => [{ :name => 'Countly', :version => '~> 1.0.0' }]}
                   crittercism = { :name => 'Crittercism', :dependencies => [{ :name => 'CrittercismSDK', :version => '~> 4.3.3' }]}
                   flurry = { :name => 'Flurry', :dependencies => [{ :name => 'FlurrySDK', :version => '~> 4.4.0' }]}
-                  google = { :name => 'GoogleAnalytics', :dependencies => [{ :name => 'GoogleAnalytics-iOS-SDK', :version => '3.0.7' }]}
+                  google = { :name => 'GoogleAnalytics', :dependencies => [{ :name => 'GoogleAnalytics-iOS-SDK', :version => '~> 3.0.7' }]}
                   localytics = { :name => 'Localytics', :dependencies => [{ :name => 'Localytics-iOS-Client', :version => '~> 2.23.0' }]}
                   mixpanel = { :name => 'Mixpanel', :dependencies => [{ :name => 'Mixpanel', :version => '~> 2.3.6' }]}
                   optimizely = { :name => 'Optimizely', :dependencies => [{ :name => 'Optimizely-iOS-SDK', :version => '~> 0.6.52' }]}
