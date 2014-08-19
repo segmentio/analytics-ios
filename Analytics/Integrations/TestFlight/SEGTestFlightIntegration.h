218b38b5d401cbb51ab9b27609e668e715a9964f
Travis Jeffery <tj@travisjeffery.com>
2014-08-18 22:40:27 -0500

    Add testflight

---
+++ Analytics/Integrations/TestFlight/SEGTestFlightIntegration.h
@@ -0,0 +1,13 @@
+//
+//  SEGTestFlightIntegration.h
+//  Analytics
+//
+//  Created by Travis Jeffery on 2014-08-18.
+//  Copyright (c) 2014 Segment.io. All rights reserved.
+//
+
+#import "SEGAnalyticsIntegration.h"
+
+@interface SEGTestFlightIntegration : SEGAnalyticsIntegration
+
+@end
