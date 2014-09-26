2ab19951bf9028b3ba38270fc66dab669339847e
Travis Jeffery <tj@travisjeffery.com>
2014-09-26 00:35:13 -0500

    Puts integration dependencies in json

--- scripts/build.rb
+++ scripts/build.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module Build
   class << self
     class Pod
@@ -6,8 +8,8 @@ module Build
       class << self
         def pod_from_hash h
           p = Pod.new
-          p.name = h[:name]
-          p.version = h[:version]
+          p.name = h["name"]
+          p.version = h["version"]
           p
         end
       end
@@ -23,8 +25,8 @@ module Build
       class << self
         def spec_from_hash h
           s = Subspec.new
-          s.name = h[:name]
-          s.dependencies = h[:dependencies].map { |d| Pod.pod_from_hash d }
+          s.name = h["name"]
+          s.dependencies = h["dependencies"].map { |d| Pod.pod_from_hash(d) }
           s
         end
       end
@@ -39,29 +41,7 @@ module Build
     end
 
     def subspecs
-      @pods ||= begin
-                  amplitude = { :name => 'Amplitude', :dependencies => [{ :name => 'Amplitude-iOS', :version => '~> 2.1.0' }]}
-                  appsflyer = { :name => 'AppsFlyer', :dependencies => [{ :name => 'AppsFlyer-SDK', :version => '~> 2.5' }]}
-                  bugsnag = { :name => 'Bugsnag', :dependencies => [{ :name => 'Bugsnag', :version => '~> 3.1.2' }]}
-                  countly = { :name => 'Countly', :dependencies => [{ :name => 'Countly', :version => '~> 1.0.0' }]}
-                  crittercism = { :name => 'Crittercism', :dependencies => [{ :name => 'CrittercismSDK', :version => '~> 4.3.3' }]}
-                  flurry = { :name => 'Flurry', :dependencies => [{ :name => 'FlurrySDK', :version => '~> 4.4.0' }]}
-                  google = { :name => 'GoogleAnalytics', :dependencies => [{ :name => 'GoogleAnalytics-iOS-SDK', :version => '3.0.9' }]}
-                  localytics = { :name => 'Localytics', :dependencies => [{ :name => 'Localytics-AMP', :version => '2.60.0' }]}
-                  mixpanel = { :name => 'Mixpanel', :dependencies => [{ :name => 'Mixpanel', :version => '2.5.3' }]}
-                  optimizely = { :name => 'Optimizely', :dependencies => [{ :name => 'Optimizely-iOS-SDK', :version => '~> 0.6.52' }]}
-                  quantcast = { :name => 'Quantcast', :dependencies => [{ :name => 'Quantcast-Measure', :version => '~> 1.4.4' }]}
-                  segmentio = { :name => 'Segmentio', :dependencies => []}
-                  taplytics = { :name => 'Taplytics', :dependencies => [{ :name => 'Taplytics', :version => '~> 1.3.0' }]}
-                  tapstream = { :name => 'Tapstream', :dependencies => [{ :name => 'Tapstream', :version => '~> 2.7' }]}
-                  testflight = { :name => 'TestFlight', :dependencies => [{ :name => 'TestFlightSDK', :version => '~> 3.0.2' }]}
-
-                  pods = [segmentio, amplitude, appsflyer, bugsnag, countly, crittercism, flurry, google, localytics, mixpanel, optimizely, quantcast, taplytics, tapstream, testflight]
-
-                  pods.map { |p| Subspec.spec_from_hash p }
-                end
-
+      @pods ||= JSON.parse(File.read(File.join(File.dirname(__FILE__), 'integrations.json')))["integrations"].map { |p| Subspec.spec_from_hash(p) }
     end
-
   end
 end
