146ed45a4e30471923d18084572532d8f4a8b73b
Travis Jeffery <tj@travisjeffery.com>
2015-01-15 20:10:08 -0500

    Upload localytics to 3.0.0

--- Analytics/Integrations/Localytics/SEGLocalyticsIntegration.m
+++ Analytics/Integrations/Localytics/SEGLocalyticsIntegration.m
@@ -2,46 +2,46 @@
 // Copyright (c) 2014 Segment.io. All rights reserved.
 
 #import "SEGLocalyticsIntegration.h"
-#import <LocalyticsSession.h>
+#import <Localytics.h>
 #import "SEGAnalyticsUtils.h"
 #import "SEGAnalytics.h"
 
 @implementation SEGLocalyticsIntegration
 
 + (BOOL) validateEmail:(NSString *)candidate {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
-    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
-
-    return [emailTest evaluateWithObject:candidate];
+  NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+  NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+  
+  return [emailTest evaluateWithObject:candidate];
 }
 
 + (void)load {
-    [SEGAnalytics registerIntegration:self withIdentifier:@"Localytics"];
+  [SEGAnalytics registerIntegration:self withIdentifier:@"Localytics"];
 }
 
 #pragma mark - Initialization
 
 - (id)init {
-    if (self = [super init]) {
-        self.name = @"Localytics";
-        self.valid = NO;
-        self.initialized = NO;
-    }
-    return self;
+  if (self = [super init]) {
+    self.name = @"Localytics";
+    self.valid = NO;
+    self.initialized = NO;
+  }
+  return self;
 }
 
 - (void)start
 {
-    NSString *appKey = [self.settings objectForKey:@"appKey"];
-
-    [[LocalyticsSession sharedLocalyticsSession] startSession:appKey];
-
-    NSNumber *sessionTimeoutInterval = [self.settings objectForKey:@"sessionTimeoutInterval"];
-    if (sessionTimeoutInterval != nil && [sessionTimeoutInterval floatValue] > 0) {
-        [LocalyticsSession sharedLocalyticsSession].sessionTimeoutInterval = [sessionTimeoutInterval floatValue];
-    }
-
-    SEGLog(@"LocalyticsIntegration initialized.");
+  NSString *appKey = [self.settings objectForKey:@"appKey"];
+  
+  [Localytics autoIntegrate:appKey launchOptions:nil];
+  
+  NSNumber *sessionTimeoutInterval = [self.settings objectForKey:@"sessionTimeoutInterval"];
+  if (sessionTimeoutInterval != nil && [sessionTimeoutInterval floatValue] > 0) {
+    [Localytics setSessionTimeoutInterval:[sessionTimeoutInterval floatValue]];
+  }
+  
+  SEGLog(@"LocalyticsIntegration initialized.");
 }
 
 
@@ -49,8 +49,8 @@
 
 - (void)validate
 {
-    BOOL hasAppKey = [self.settings objectForKey:@"appKey"] != nil;
-    self.valid = hasAppKey;
+  BOOL hasAppKey = [self.settings objectForKey:@"appKey"] != nil;
+  self.valid = hasAppKey;
 }
 
 
@@ -58,60 +58,60 @@
 
 - (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options
 {
-    if (userId) {
-        [[LocalyticsSession sharedLocalyticsSession] setCustomerId:userId];
-    }
-
-    // Email
-    NSString *email = [traits objectForKey:@"email"];
-    if (!email && [SEGLocalyticsIntegration validateEmail:userId]) {
-        email = userId;
-    }
-    if (email) {
-        [[LocalyticsSession sharedLocalyticsSession] setCustomerEmail:email];
-    }
-
-    // Name
-    NSString *name = [traits objectForKey:@"name"];
-    // TODO support first name, last name?
-    if (name) {
-        [[LocalyticsSession sharedLocalyticsSession] setCustomerName:name];
-    }
-
-    // Other traits. Iterate over all the traits and set them.
-    for (NSString *key in traits) {
-        NSString* traitValue = [NSString stringWithFormat:@"%@", [traits objectForKey:key]];
-        [[LocalyticsSession sharedLocalyticsSession] setValueForIdentifier:key value:traitValue];
-    }
+  if (userId) {
+    [Localytics setCustomerId:userId];
+  }
+  
+  // Email
+  NSString *email = [traits objectForKey:@"email"];
+  if (!email && [SEGLocalyticsIntegration validateEmail:userId]) {
+    email = userId;
+  }
+  if (email) {
+    [Localytics setValue:email forIdentifier:@"email"];
+  }
+  
+  // Name
+  NSString *name = [traits objectForKey:@"name"];
+  // TODO support first name, last name?
+  if (name) {
+    [Localytics setValue:name forIdentifier:@"customer_name"];
+  }
+  
+  // Other traits. Iterate over all the traits and set them.
+  for (NSString *key in traits) {
+    NSString* traitValue = [NSString stringWithFormat:@"%@", [traits objectForKey:key]];
+    [Localytics setValue:traitValue forIdentifier:key];
+  }
 }
 
 - (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options
 {
-    // TODO add support for dimensions
-    // TODO add support for value
-
-    // Backgrounded? Restart the session to add this event.
-    BOOL isBackgrounded = [[UIApplication sharedApplication] applicationState] != UIApplicationStateActive;
-    if (isBackgrounded) {
-        [[LocalyticsSession sharedLocalyticsSession] resume];
-    }
-
-    [[LocalyticsSession sharedLocalyticsSession] tagEvent:event attributes:properties];
-
-    // Backgrounded? Close the session again after the event.
-    if (isBackgrounded) {
-        [[LocalyticsSession sharedLocalyticsSession] close];
-    }
+  // TODO add support for dimensions
+  // TODO add support for value
+  
+  // Backgrounded? Restart the session to add this event.
+  BOOL isBackgrounded = [[UIApplication sharedApplication] applicationState] != UIApplicationStateActive;
+  if (isBackgrounded) {
+    [Localytics openSession];
+  }
+  
+  [Localytics tagEvent:event attributes:properties];
+  
+  // Backgrounded? Close the session again after the event.
+  if (isBackgrounded) {
+    [Localytics closeSession];
+  }
 }
 
 - (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options
 {
-    // For enterprise only...
-    [[LocalyticsSession sharedLocalyticsSession] tagScreen:screenTitle];
+  // For enterprise only...
+  [Localytics tagScreen:screenTitle];
 }
 
 - (void)registerForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken options:(NSDictionary *)options {
-    [[LocalyticsSession sharedLocalyticsSession] setPushToken:deviceToken];
+  [Localytics setPushToken:deviceToken];
 }
 
 
@@ -119,30 +119,30 @@
 
 - (void)applicationDidEnterBackground
 {
-    [[LocalyticsSession sharedLocalyticsSession] close];
-    [[LocalyticsSession sharedLocalyticsSession] upload];
+  [Localytics closeSession];
+  [Localytics upload];
 }
 
 - (void)applicationWillEnterForeground
 {
-    [[LocalyticsSession sharedLocalyticsSession] resume];
-    [[LocalyticsSession sharedLocalyticsSession] upload];
+  [Localytics openSession];
+  [Localytics upload];
 }
 
 - (void)applicationWillTerminate
 {
-    [[LocalyticsSession sharedLocalyticsSession] close];
-    [[LocalyticsSession sharedLocalyticsSession] upload];
+  [Localytics closeSession];
+  [Localytics upload];
 }
 - (void)applicationWillResignActive
 {
-    [[LocalyticsSession sharedLocalyticsSession] close];
-    [[LocalyticsSession sharedLocalyticsSession] upload];
+  [Localytics closeSession];
+  [Localytics upload];
 }
 - (void)applicationDidBecomeActive
 {
-    [[LocalyticsSession sharedLocalyticsSession] resume];
-    [[LocalyticsSession sharedLocalyticsSession] upload];
+  [Localytics openSession];
+  [Localytics upload];
 }
 
 
