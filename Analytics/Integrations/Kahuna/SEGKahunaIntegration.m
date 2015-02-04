149a7f9f59b3654d715e9d6baceff9e544db483a
Travis Jeffery <tj@travisjeffery.com>
2015-02-04 00:18:28 -0500

    Add kahuna

--- Analytics/Integrations/Kahuna/SEGKahunaIntegration.m
+++ Analytics/Integrations/Kahuna/SEGKahunaIntegration.m
@@ -1,15 +1,76 @@
-
-// KahunaIntegration.h
+// KahunaIntegration.m
 // Copyright (c) 2014 Segment.io. All rights reserved.
 
-#import <Foundation/Foundation.h>
-#import "SEGAnalyticsIntegration.h"
+#import "SEGKahunaIntegration.h"
+#import "KahunaAnalytics.h"
+#import "SEGAnalyticsUtils.h"
+#import "SEGAnalytics.h"
 
-@interface SEGKahunaIntegration : SEGAnalyticsIntegration
+@implementation SEGKahunaIntegration
 
-@property(nonatomic, copy) NSString *name;
-@property(nonatomic, assign) BOOL valid;
-@property(nonatomic, assign) BOOL initialized;
-@property(nonatomic, copy) NSDictionary *settings;
+#pragma mark - Initialization
+
++ (void)load {
+  [SEGAnalytics registerIntegration:self withIdentifier:@"Kahuna"];
+}
+
+- (id)init {
+  if (self = [super init]) {
+    self.name = @"Kahuna";
+    self.valid = NO;
+    self.initialized = NO;
+  }
+  return self;
+}
+
+- (void)start {
+  [KahunaAnalytics startWithKey:[self.settings objectForKey:@"apiKey"]];
+  [super start];
+}
+
+
+#pragma mark - Settings
+
+- (void)validate {
+  self.valid = [self.settings objectForKey:@"apiKey"] != nil;
+}
+
+#pragma mark - Analytics API
+
+- (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options {
+  [KahunaAnalytics setUserCredentialsWithKey:KAHUNA_CREDENTIAL_USERNAME andValue:userId];
+  NSString *email = [self.class extractEmail:userId traits:traits];
+  if (email) {
+    [KahunaAnalytics setUserCredentialsWithKey:KAHUNA_CREDENTIAL_EMAIL andValue:email];
+  }
+  [KahunaAnalytics setUserAttributes:traits];
+}
+
+- (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options {
+  NSNumber *revenue = [self.class extractRevenue:properties];
+  if (revenue != nil){
+    NSNumber *quantity = [properties objectForKey:@"quantity"] ?: @1;
+    [KahunaAnalytics trackEvent:event withCount:[quantity longValue] andValue:[revenue longValue]];
+  }
+}
+
+- (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options {
+  // Track the screen view as an event.
+  [self track:SEGEventNameForScreenTitle(screenTitle) properties:properties options:options];
+}
+
+- (void)registerForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken options:(NSDictionary *)options {
+  [KahunaAnalytics setDeviceToken:deviceToken];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+  UIApplication *application = [UIApplication sharedApplication];
+  [KahunaAnalytics handleNotification:[[notification userInfo] valueForKey:
+                                       UIApplicationLaunchOptionsRemoteNotificationKey] withApplicationState:[application applicationState]];
+}
+
+- (void)reset {
+  [KahunaAnalytics logout];
+}
 
 @end
