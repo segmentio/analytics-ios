f029917e16843f366f8b960cd00fef3a7785b817
Dustin L. Howett <dhowett@apptimize.com>
2015-04-29 12:08:45 -0700

    Add Apptimize Integration.
    - SEGApptimizeIntegration.
    - Copy the Apptimize headers, statically link the framework binary.

---
+++ Analytics/Integrations/Apptimize/SEGApptimizeIntegration.m
@@ -0,0 +1,63 @@
+//
+//  SEGApptimizeIntegration.m
+//  Analytics
+//
+//  Created by Dustin L. Howett on 3/30/15.
+//  Copyright (c) 2015 Segment.io. All rights reserved.
+//
+
+#import "SEGApptimizeIntegration.h"
+
+#import "SEGAnalyticsUtils.h"
+#import "SEGAnalytics.h"
+
+#import <Apptimize/Apptimize.h>
+#import <Apptimize/Apptimize+Segment.h>
+
+@implementation SEGApptimizeIntegration
+
++ (NSString *)name { return @"Apptimize"; }
+
++ (void)load {
+    [SEGAnalytics registerIntegration:self withIdentifier:[self name]];
+}
+
+- (id)init {
+    if (self = [super init]) {
+        self.name = [[self class] name];
+        self.valid = NO;
+        self.initialized = NO;
+        
+        [Apptimize SEG_ensureLibraryHasBeenInitialized];
+    }
+    return self;
+}
+
+- (void)validate {
+    self.valid = [self.settings objectForKey:@"appkey"] != nil;
+}
+
+- (void)start {
+    [Apptimize startApptimizeWithApplicationKey:[self.settings objectForKey:@"appkey"]];
+    [super start];
+}
+
+- (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options {
+    if (userId != nil) {
+        [Apptimize setUserAttributeString:userId forKey:@"user_id"];
+    }
+    
+    if (traits) {
+        [Apptimize SEG_setUserAttributesFromDictionary:traits];
+    }
+}
+
+- (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options {
+    [Apptimize SEG_track:event attributes:properties];
+}
+
+- (void)reset {
+    [Apptimize SEG_resetUserData];
+}
+
+@end
