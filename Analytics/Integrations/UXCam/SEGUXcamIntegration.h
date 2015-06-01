b01973f6f19be8e2f6d6dd6d3dcf5c6e4cf6a4dd
Richard Groves <richard@noodlfroot.com>
2015-06-01 15:29:32 +0100

    Adding UXCam integration
    First pass at adding UXCam integration

---
+++ Analytics/Integrations/UXCam/SEGUXcamIntegration.h
@@ -0,0 +1,13 @@
+//
+//  SEGUXcamIntegration.h
+//  Analytics
+//
+//  Created by Richard Groves on 01/06/2015.
+//  Copyright (c) 2015 Segment.io. All rights reserved.
+//
+
+#import "SEGAnalyticsIntegration.h"
+
+@interface SEGUXcamIntegration : SEGAnalyticsIntegration
+
+@end
