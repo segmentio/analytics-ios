26b03a2ee78c0edc82fe5ae855e1cb83c44b2e29
Prateek Srivastava <f2prateek@gmail.com>
2015-07-01 15:59:57 -0400

    Add docs

--- Analytics/Integrations/Mixpanel/SEGMixpanelIntegration.m
+++ Analytics/Integrations/Mixpanel/SEGMixpanelIntegration.m
@@ -1,58 +1,76 @@
 // MixpanelIntegration.m
 // Copyright (c) 2014 Segment.io. All rights reserved.
 
+// Import the public header files
 #import "SEGMixpanelIntegration.h"
+// Import the Mixpanel SDK
 #import <Mixpanel.h>
-#import "SEGAnalyticsUtils.h"
+// Import the SEGAnalytics class
 #import "SEGAnalytics.h"
+// Import some utility methods
+#import "SEGAnalyticsUtils.h"
 
-
+// Implementation for our integration in SEGMixpanel.h
+// See SEGAnalyticsIntegration.h to see which methods should be overriden for what purpose.
 @implementation SEGMixpanelIntegration
 
 #pragma mark - Initialization
 
+// The load method is automatically invoked when the class is added to the Objective-C Runtime.
+// @see https://developer.apple.com/library/prerelease/ios/documentation/Cocoa/Reference/Foundation/Classes/NSObject_Class/index.html#//apple_ref/occ/clm/NSObject/load
 + (void)load
 {
+    // We use this to register an integration with the right key/identifier, which should be the same as in the Segment metadata.
     [SEGAnalytics registerIntegration:self withIdentifier:@"Mixpanel"];
 }
 
+// The initialize method is called once when the integrations are loaded (but not initialized).
 - (id)init
 {
     if (self = [super init]) {
+        // Same as the Mixpanel identifier/key in the Segment metadata.
         self.name = @"Mixpanel";
+        // Invalid and uninitialized by default.
         self.valid = NO;
         self.initialized = NO;
+        // Use the real Mixpanel class for normal operation.
         self.mixpanelClass = [Mixpanel class];
     }
     return self;
 }
 
-- (void)start
-{
-    NSString *token = [self.settings objectForKey:@"token"];
-    [self.mixpanelClass sharedInstanceWithToken:token];
-
-    [super start];
-}
-
-
-#pragma mark - Settings
-
+// Validate any settings provided to the integration, such as required keys.
 - (void)validate
 {
+    // For example, Mixpanel requires at least the `token` to be set.
     BOOL hasToken = [self.settings objectForKey:@"token"] != nil;
+    // Set valid to YES or NO depending on the whether all the settings are avilable or not.
     self.valid = hasToken;
+}
+
+// The start method is called to initialize an integration, after it has been validated.
+- (void)start
+{
+    // Grab any required and optional settings.
+    NSString *token = [self.settings objectForKey:@"token"];
+
+    // Initialize the Mixpanel SDK.
+    [self.mixpanelClass sharedInstanceWithToken:token];
+
+    // Call the super implementation, which will notify observers that Mixpanel has been initialized.
+    [super start];
 }
 
 #pragma mark - Analytics API
 
 - (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options
 {
+    // Ensure that the userID is set and valid (i.e. a non-empty string).
     if (userId != nil && [userId length] != 0) {
         [[self.mixpanelClass sharedInstance] identify:userId];
     }
 
-    // Map the traits to special mixpanel keywords.
+    // Map the traits to special mixpanel properties.
     NSDictionary *map = [NSDictionary dictionaryWithObjectsAndKeys:
                                           @"$first_name", @"firstName",
                                           @"$last_name", @"lastName",
@@ -62,12 +80,15 @@
                                           @"$name", @"name",
                                           @"$username", @"username",
                                           @"$phone", @"phone", nil];
-
     NSDictionary *mappedTraits = [SEGAnalyticsIntegration map:traits withMap:map];
 
+    // Register the mapped traits.
     [[self.mixpanelClass sharedInstance] registerSuperProperties:mappedTraits];
 
+    // Mixpanel also has a people API that works seperately, so we set the traits for it as well.
     if ([(NSNumber *)[self.settings objectForKey:@"people"] boolValue]) {
+        // You'll notice that we could also have done: [self.mixpanelClass.sharedInstance.people set:mappedTraits];
+        // Using methods instead of properties directly lets us mock them in tests, which is why we use the syntax below.
         [[[self.mixpanelClass sharedInstance] people] set:mappedTraits];
     }
 }
@@ -77,13 +98,20 @@
     // Track the raw event.
     [[self.mixpanelClass sharedInstance] track:event properties:properties];
 
-    // If revenue is included and People is enabled, trackCharge to Mixpanel.
+    // Don't go any further if Mixpanel people is disabled.
+    if (![(NSNumber *)[self.settings objectForKey:@"people"] boolValue]) {
+        return;
+    }
+
+    // Extract the revenue from the properties passed in to us.
     NSNumber *revenue = [SEGAnalyticsIntegration extractRevenue:properties];
-    if (revenue && [(NSNumber *)[self.settings objectForKey:@"people"] boolValue]) {
+    // Check if there was a revenue.
+    if (revenue) {
         [[[self.mixpanelClass sharedInstance] people] trackCharge:revenue];
     }
 
-    // If people is enabled we may want to increment this event in people
+    // Mixpanel has the ability keep a running 'count' events. So we check if this is an event
+    // that should be incremented (by checking the settings).
     if ([self eventShouldIncrement:event]) {
         [[[self.mixpanelClass sharedInstance] people] increment:event by:@1];
         NSString *lastEvent = [NSString stringWithFormat:@"Last %@", event];
@@ -93,22 +121,28 @@
 
 - (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options
 {
+    // Mixpanel doesn't have the concept of screen views, so we only track the trackAllPages option is enabled.
     if ([(NSNumber *)[self.settings objectForKey:@"trackAllPages"] boolValue]) {
         // Track the screen view as an event.
+        // e.g. "Home" screen is tracked as "Viewed Home Screen".
         [self track:SEGEventNameForScreenTitle(screenTitle) properties:properties options:options];
     }
 }
 
 - (void)alias:(NSString *)newId options:(NSDictionary *)options
 {
+    // Instead of using our own anonymousId, we use Mixpanel's own generated Id.
     [[self.mixpanelClass sharedInstance] createAlias:newId forDistinctID:[[self.mixpanelClass sharedInstance] distinctId]];
 }
 
+// Invoked when the device is registered with a push token.
+// Mixpanel uses this to send push messages to the device, so forward it to Mixpanel.
 - (void)registerForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken options:(NSDictionary *)options
 {
     [[[self.mixpanelClass sharedInstance] people] addPushDeviceToken:deviceToken];
 }
 
+// An internal utility method that checks the settings to see if this event should be incremented in Mixpanel.
 - (BOOL)eventShouldIncrement:(NSString *)event
 {
     NSArray *increments = [self.settings objectForKey:@"increments"];
@@ -130,5 +164,7 @@
 {
     [[self.mixpanelClass sharedInstance] flush];
 }
+
+// Mixpanel doesn't implement the group method so we don't implement it!
 
 @end
--- Analytics/SEGAnalyticsIntegration.h
+++ Analytics/SEGAnalyticsIntegration.h
@@ -23,12 +23,46 @@ extern NSString *SEGAnalyticsIntegrationDidStart;
 // Analytics API
 // -------------
 
+
+// Identify will be called when the user calls either of the following:
+// 1. [[SEGAnalytics sharedInstance] identify:someUserId];
+// 2. [[SEGAnalytics sharedInstance] identify:someUserId traits:someTraits];
+// 3. [[SEGAnalytics sharedInstance] identify:someUserId traits:someTraits options:someOptions];
+// @see https://segment.com/docs/spec/identify/
 - (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options;
+
+// Track will be called when the user calls either of the following:
+// 1. [[SEGAnalytics sharedInstance] track:someEvent];
+// 2. [[SEGAnalytics sharedInstance] track:someEvent properties:someProperties];
+// 3. [[SEGAnalytics sharedInstance] track:someEvent properties:someProperties options:someOptions];
+// @see https://segment.com/docs/spec/track/
 - (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options;
+
+// Screen will be called when the user calls either of the following:
+// 1. [[SEGAnalytics sharedInstance] screen:someEvent];
+// 2. [[SEGAnalytics sharedInstance] screen:someEvent properties:someProperties];
+// 3. [[SEGAnalytics sharedInstance] screen:someEvent properties:someProperties options:someOptions];
+// @see https://segment.com/docs/spec/screen/
 - (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options;
+
+
+// Group will be called when the user calls either of the following:
+// 1. [[SEGAnalytics sharedInstance] group:someGroupId];
+// 2. [[SEGAnalytics sharedInstance] group:someGroupId traits:];
+// 3. [[SEGAnalytics sharedInstance] group:someGroupId traits:someGroupTraits options:someOptions];
+// @see https://segment.com/docs/spec/group/
 - (void)group:(NSString *)groupId traits:(NSDictionary *)traits options:(NSDictionary *)options;
+
+// Alias will be called when the user calls either of the following:
+// 1. [[SEGAnalytics sharedInstance] alias:someNewId];
+// 2. [[SEGAnalytics sharedInstance] alias:someNewId options:someOptions];
+// @see https://segment.com/docs/spec/alias/
 - (void)alias:(NSString *)newId options:(NSDictionary *)options;
+
+// Reset is invoked when the user logs out, and any data saved about the user should be cleared.
 - (void)reset;
+
+// Flush is invoked when any queued events should be uploaded.
 - (void)flush;
 
 @optional
