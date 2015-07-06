1b7a6af7cddf17d28b0da07b26015852299da45c
Richard Groves <richard@noodlfroot.com>
2015-07-06 19:45:14 +0100

    UXCam SDK integration
    NB: Will not build as is due to a version problem with Countly - change their SDK version to 2.0.0 in integrations.json rather than 3.0.0 until they fix their CocoaPod.
    
    Adding UXCam SDK to integrations.json
    Adding SEGUXCamIntegration
    Adding SEGUXCamIntegrationTests - very simple work in progress
    Editing AnalyticsTest and settings.json files to add UXCam integration

---
+++ iOS Tests/Integrations/UXCam/SEGUXCamIntegrationTests.m
@@ -0,0 +1,47 @@
+//
+//  SEGOptimizelyIntegrationTests.m
+//  Analytics
+//
+//  Created by Ricahrd Groves on 2015-07-06.
+//  Copyright (c) 2015 Segment.io. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "SEGUXCamIntegration.h"
+#import <UXCam/UXCam.h>
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
+
+@interface SEGUXCamIntegrationTests : XCTestCase
+
+@property SEGUXCamIntegration *integration;
+@property Class uxcamClassMock;
+
+@end
+
+
+@implementation SEGUXCamIntegrationTests
+
+- (void)setUp
+{
+    [super setUp];
+
+    _uxcamClassMock = mockClass([UXCam class]);
+
+    _integration = [[SEGUXCamIntegration alloc] init];
+    [_integration setUxcamClass:_uxcamClassMock];
+}
+
+- (void)testValidate
+{
+	[_integration updateSettings:@{ @"apiKey" : @"foo" }];
+	
+	XCTAssertTrue(_integration.valid);
+}
+
+@end
