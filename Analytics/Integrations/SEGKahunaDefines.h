eb4024c243cf2c91d4a5b060e6227bd1ee861647
sharekris <ksubramanian73@gmail.com>
2015-07-29 14:01:16 -0700

    Squashed commit of the following:
    commit fae7de26601890eb56948bf23a4132f0c64eba22
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Wed Jul 29 13:48:49 2015 -0700
    
        Podfile.lock changed to use 2.0.3 after running make build.
    
    commit 0d777e823a139e0c75ad62fc50855994e440f4d2
    Merge: f2e88e8 12165d5
    Author: SimpliRay <rayleeriver@gmail.com>
    Date:   Wed Jul 29 10:27:25 2015 -0700
    
        Merge pull request #1 from sharekris/updating_code_for_2.0.2
    
        Updating code for 2.0.3
    
    commit 12165d52362b697cc89111b86a9c22c713ab4524
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Tue Jul 28 17:06:28 2015 -0700
    
        Removed tab.
    
    commit f99b41841f006aea4a22bce3c22215a33cf02ce0
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Tue Jul 28 16:39:56 2015 -0700
    
        Changed to version 2.0.3 as that is the latest.
    
    commit 62c767323ef97efaab539c315e78f97867b95b67
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Tue Jul 28 16:35:31 2015 -0700
    
        Added call to setSDKWrapper method to send the wrapper name and version to our server.
        Modified the track event with quantity and revenue to trackEvent with count and value only when both revenue and quantity are specified in the properties.
    
    commit b9d621b39dcb3c613b02277be8b04c3d50ab1e87
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Tue Jul 28 12:25:05 2015 -0700
    
        Removed commented out tests.
    
    commit b2dc396bffa956fa16cffa7e8dd387100bf33868
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Mon Jul 27 19:24:08 2015 -0700
    
        Added OCMokito tests for Kahuna Integration. Made some code changes in SEGKahunaIntegration to allow OCMokito to mock the Kahuna object.
        Created SEGKahunaDefines.h header file and moved all static string definitions into that file.
    
    commit d34eb4aa2482b4f26730dfa530ccabd04d68c7e1
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Thu Jul 23 15:25:24 2015 -0700
    
        Modified getUserCredentials to use createUserCredentials. The latter is what we want to use since we want the request to identify detect non-overlaps. If we do a getUserCredential then it will always be over lapping credentials.
    
    commit 59d2467213eb62bab230acceb1a7e6adb3e43d3b
    Author: sharekris <ksubramanian73@gmail.com>
    Date:   Mon Jul 20 13:35:38 2015 -0700
    
        Modified the Kahuna wrapper to use Kahuna instead of KahunaAnalytics SDK. Replaced the API setUserCredentialsWithKey with addCredential and then calling loginWithCredentials.

---
+++ Analytics/Integrations/SEGKahunaDefines.h
@@ -0,0 +1,28 @@
+// SEGKahunaDefines.h
+// Copyright (c) 2014 Segment.io. All rights reserved.
+
+#ifndef Analytics_SEGKahunaDefines_h
+#define Analytics_SEGKahunaDefines_h
+
+static NSString *const KAHUNA_VIEWED_PRODUCT_CATEGORY = @"Viewed Product Category";
+static NSString *const KAHUNA_VIEWED_PRODUCT = @"Viewed Product";
+static NSString *const KAHUNA_ADDED_PRODUCT = @"Added Product";
+static NSString *const KAHUNA_COMPLETED_ORDER = @"Completed Order";
+
+static NSString *const KAHUNA_LAST_VIEWED_CATEGORY = @"Last Viewed Category";
+static NSString *const KAHUNA_CATEGORIES_VIEWED = @"Categories Viewed";
+static NSString *const KAHUNA_LAST_PRODUCT_VIEWED_NAME = @"Last Product Viewed Name";
+static NSString *const KAHUNA_LAST_PRODUCT_VIEWED_ID = @"Last Produced Viewed Id";
+static NSString *const KAHUNA_LAST_PRODUCT_ADDED_TO_CART_NAME = @"Last Product Added To Cart Name";
+static NSString *const KAHUNA_LAST_PRODUCT_ADDED_TO_CART_CATEGORY = @"Last Product Added To Cart Category";
+static NSString *const KAHUNA_LAST_PURCHASE_DISCOUNT = @"Last Purchase Discount";
+
+static NSString *const KAHUNA_CATEGORY = @"category";
+static NSString *const KAHUNA_NAME = @"name";
+static NSString *const KAHUNA_ID = @"id";
+static NSString *const KAHUNA_DISCOUNT = @"discount";
+static NSString *const KAHUNA_NONE = @"None";
+static NSString *const SEGMENT = @"segment";
+
+
+#endif
