97c87d81f221ff3b3b17dc579df404f980754585
Prateek Srivastava <f2prateek@gmail.com>
2015-11-02 14:04:59 -0800

    Initial commit

---
+++ Example/Analytics/SEGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SEGAppDelegate.h
+//  Analytics
+//
+//  Created by Prateek Srivastava on 11/02/2015.
+//  Copyright (c) 2015 Prateek Srivastava. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SEGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Analytics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Analytics
+//
+//  Created by Prateek Srivastava on 11/02/2015.
+//  Copyright (c) 2015 Prateek Srivastava. All rights reserved.
+//
+
+@import UIKit;
+#import "SEGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SEGAppDelegate class]));
+    }
+}
