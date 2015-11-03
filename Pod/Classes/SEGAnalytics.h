8a21cfcf960af2112a664cf4efe20c4160f4362b
Prateek Srivastava <f2prateek@gmail.com>
2015-11-02 17:30:58 -0800

    analytics API

--- Pod/Classes/SEGAnalytics.h
+++ Pod/Classes/SEGAnalytics.h
@@ -43,4 +43,95 @@
 
 @interface SEGAnalytics : NSObject
 
+/*!
+ @method
+ 
+ @abstract
+ Associate a user with their unique ID and record traits about them.
+ 
+ @param userId        A database ID (or email address) for this user. If you don't have a userId
+ but want to record traits, you should pass nil. For more information on how we
+ generate the UUID and Apple's policies on IDs, see https://segment.io/libraries/ios#ids
+ 
+ @param traits        A dictionary of traits you know about the user. Things like: email, name, plan, etc.
+ 
+ @discussion
+ When you learn more about who your user is, you can record that information with identify.
+ */
+- (void)identify:(NSString *)userId;
+- (void)identify:(NSString *)userId traits:(NSDictionary *)traits;
+- (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options;
+
+/*!
+ @method
+ 
+ @abstract
+ Record the actions your users perform.
+ 
+ @param event         The name of the event you're tracking. We recommend using human-readable names
+ like `Played a Song` or `Updated Status`.
+ 
+ @param properties    A dictionary of properties for the event. If the event was 'Added to Shopping Cart', it might
+ have properties like price, productType, etc.
+ 
+ @discussion
+ When a user performs an action in your app, you'll want to track that action for later analysis. Use the event name to say what the user did, and properties to specify any interesting details of the action.
+ */
+- (void)track:(NSString *)event;
+- (void)track:(NSString *)event properties:(NSDictionary *)properties;
+- (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options;
+
+/*!
+ @method
+ 
+ @abstract
+ Record the screens or views your users see.
+ 
+ @param screenTitle   The title of the screen being viewed. We recommend using human-readable names
+ like 'Photo Feed' or 'Completed Purchase Screen'.
+ 
+ @param properties    A dictionary of properties for the screen view event. If the event was 'Added to Shopping Cart',
+ it might have properties like price, productType, etc.
+ 
+ @discussion
+ When a user views a screen in your app, you'll want to record that here. For some tools like Google Analytics and Flurry, screen views are treated specially, and are different from "events" kind of like "page views" on the web. For services that don't treat "screen views" specially, we map "screen" straight to "track" with the same parameters. For example, Mixpanel doesn't treat "screen views" any differently. So a call to "screen" will be tracked as a normal event in Mixpanel, but get sent to Google Analytics and Flurry as a "screen".
+ */
+- (void)screen:(NSString *)screenTitle;
+- (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties;
+- (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options;
+
+/*!
+ @method
+ 
+ @abstract
+ Associate a user with a group, organization, company, project, or w/e *you* call them.
+ 
+ @param groupId       A database ID for this group.
+ 
+ @param traits        A dictionary of traits you know about the group. Things like: name, employees, etc.
+ 
+ @discussion
+ When you learn more about who the group is, you can record that information with group.
+ */
+- (void)group:(NSString *)groupId;
+- (void)group:(NSString *)groupId traits:(NSDictionary *)traits;
+- (void)group:(NSString *)groupId traits:(NSDictionary *)traits options:(NSDictionary *)options;
+
+/*!
+ @method
+ 
+ @abstract
+ Merge two user identities, effectively connecting two sets of user data as one.
+ This may not be supported by all integrations.
+ 
+ @param newId         The new ID you want to alias the existing ID to. The existing ID will be either the
+ previousId if you have called identify, or the anonymous ID.
+ 
+ @discussion
+ When you learn more about who the group is, you can record that information with group.
+ */
+- (void)alias:(NSString *)newId;
+- (void)alias:(NSString *)newId options:(NSDictionary *)options;
+
+
 @end
--- Pod/Classes/SEGAnalytics.m
+++ Pod/Classes/SEGAnalytics.m
@@ -27,12 +27,83 @@
     return self;
 }
 
--(void)use:(id<SEGIntegrationFactory>)factory{
+-(void)use:(id<SEGIntegrationFactory>)factory
+{
     [_factories addObject:factory];
 }
 
 @end
 
 @implementation SEGAnalytics
+
+- (void)identify:(NSString *)userId
+{
+    [self identify:userId traits:nil];
+}
+
+- (void)identify:(NSString *)userId traits:(NSDictionary *)traits
+{
+    [self identify:userId traits:traits options:nil];
+}
+
+- (void)identify:(NSString *)userId traits:(NSDictionary *)traits options:(NSDictionary *)options
+{
+    // todo:
+}
+
+- (void)track:(NSString *)event
+{
+    [self track:event properties:nil];
+}
+
+- (void)track:(NSString *)event properties:(NSDictionary *)properties
+{
+    [self track:event properties:properties options:nil];
+}
+
+- (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options
+{
+    // todo:
+}
+
+- (void)group:(NSString *)groupId
+{
+    [self group:groupId traits:nil];
+}
+
+- (void)group:(NSString *)groupId traits:(NSDictionary *)traits
+{
+    [self group:groupId traits:traits options:nil];
+}
+
+- (void)group:(NSString *)groupId traits:(NSDictionary *)traits options:(NSDictionary *)options
+{
+    // todo:
+}
+
+- (void)screen:(NSString *)name;
+{
+    [self screen:name properties:nil];
+}
+
+- (void)screen:(NSString *)name properties:(NSDictionary *)properties;
+{
+    [self screen:name properties:properties options:nil];
+}
+
+- (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options
+{
+    // todo:
+}
+
+- (void)alias:(NSString *)newId
+{
+    [self alias:newId options:nil];
+}
+
+- (void)alias:(NSString *)newId options:(NSDictionary *)options
+{
+    
+}
 
 @end
