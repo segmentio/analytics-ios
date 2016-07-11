d5ec4ee8f0ef4e947d5b0dad473e9421002981b7
Prateek Srivastava <f2prateek@gmail.com>
2016-07-11 14:42:55 -0600

    Record entire context for every event. Brings back 98a4672

--- Analytics/Classes/Internal/SEGSegmentIntegration.m
+++ Analytics/Classes/Internal/SEGSegmentIntegration.m
@@ -60,7 +60,7 @@ static BOOL GetAdTrackingEnabled()
 @interface SEGSegmentIntegration ()
 
 @property (nonatomic, strong) NSMutableArray *queue;
-@property (nonatomic, strong) NSDictionary *context;
+@property (nonatomic, strong) NSDictionary *cachedStaticContext;
 @property (nonatomic, strong) NSArray *batch;
 @property (nonatomic, strong) SEGAnalyticsRequest *request;
 @property (nonatomic, assign) UIBackgroundTaskIdentifier flushTaskID;
@@ -89,7 +89,7 @@ static BOOL GetAdTrackingEnabled()
         self.bluetooth = [[SEGBluetooth alloc] init];
         self.reachability = [SEGReachability reachabilityWithHostname:@"google.com"];
         [self.reachability startNotifier];
-        self.context = [self staticContext];
+        self.cachedStaticContext = [self staticContext];
         self.serialQueue = seg_dispatch_queue_create_specific("io.segment.analytics.segmentio", DISPATCH_QUEUE_SERIAL);
         self.flushTaskID = UIBackgroundTaskInvalid;
         self.analytics = analytics;
@@ -217,9 +217,6 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 - (NSDictionary *)liveContext
 {
     NSMutableDictionary *context = [[NSMutableDictionary alloc] init];
-
-    [context addEntriesFromDictionary:self.context];
-
     context[@"locale"] = [NSString stringWithFormat:
                                        @"%@-%@",
                                        [NSLocale.currentLocale objectForKey:NSLocaleLanguageCode],
@@ -405,7 +402,7 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
     for (NSUInteger i = 0; i < deviceToken.length; i++) {
         [token appendString:[NSString stringWithFormat:@"%02lx", (unsigned long)buffer[i]]];
     }
-    [self.context[@"device"] setObject:[token copy] forKey:@"token"];
+    [self.cachedStaticContext[@"device"] setObject:[token copy] forKey:@"token"];
 }
 
 - (void)continueUserActivity:(NSUserActivity *)activity
@@ -460,11 +457,13 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
 
         [payload setValue:[self integrationsDictionary:integrations] forKey:@"integrations"];
 
-        NSDictionary *defaultContext = [self liveContext];
+        NSDictionary *staticContext = self.cachedStaticContext;
+        NSDictionary *liveContext = [self liveContext];
         NSDictionary *customContext = context;
-        NSMutableDictionary *context = [NSMutableDictionary dictionaryWithCapacity:customContext.count + defaultContext.count];
-        [context addEntriesFromDictionary:defaultContext];
-        [context addEntriesFromDictionary:customContext]; // let the custom context override ours
+        NSMutableDictionary *context = [NSMutableDictionary dictionaryWithCapacity:staticContext.count + liveContext.count + customContext.count];
+        [context addEntriesFromDictionary:staticContext];
+        [context addEntriesFromDictionary:liveContext];
+        [context addEntriesFromDictionary:customContext];
         [payload setValue:[context copy] forKey:@"context"];
 
         SEGLog(@"%@ Enqueueing action: %@", self, payload);
@@ -513,7 +512,6 @@ static CTTelephonyNetworkInfo *_telephonyNetworkInfo;
         NSMutableDictionary *payloadDictionary = [[NSMutableDictionary alloc] init];
         [payloadDictionary setObject:self.configuration.writeKey forKey:@"writeKey"];
         [payloadDictionary setObject:iso8601FormattedString([NSDate date]) forKey:@"sentAt"];
-        [payloadDictionary setObject:self.context forKey:@"context"];
         [payloadDictionary setObject:self.batch forKey:@"batch"];
 
         SEGLog(@"Flushing payload %@", payloadDictionary);
