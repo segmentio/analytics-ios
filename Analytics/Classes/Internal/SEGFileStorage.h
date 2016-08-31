744fcab9677987c8c0bbc410446ef23132dd9623
Prateek Srivastava <f2prateek@gmail.com>
2016-08-31 17:55:29 -0600

    Reformat

--- Analytics/Classes/Internal/SEGFileStorage.h
+++ Analytics/Classes/Internal/SEGFileStorage.h
@@ -8,15 +8,16 @@
 #import <Foundation/Foundation.h>
 #import "SEGStorage.h"
 
+
 @interface SEGFileStorage : NSObject <SEGStorage>
 
 @property (nonatomic, strong, nullable) id<SEGCrypto> crypto;
 
 - (instancetype _Nonnull)init;
-- (instancetype _Nonnull)initWithFolder:(NSURL * _Nonnull)folderURL crypto:(id<SEGCrypto> _Nullable)crypto;
+- (instancetype _Nonnull)initWithFolder:(NSURL *_Nonnull)folderURL crypto:(id<SEGCrypto> _Nullable)crypto;
 
-- (NSURL * _Nonnull)urlForKey:(NSString * _Nonnull)key;
+- (NSURL *_Nonnull)urlForKey:(NSString *_Nonnull)key;
 
-+ (NSURL * _Nullable)applicationSupportDirectoryURL;
++ (NSURL *_Nullable)applicationSupportDirectoryURL;
 
 @end
--- Analytics/Classes/Internal/SEGUtils.h
+++ Analytics/Classes/Internal/SEGUtils.h
@@ -6,9 +6,10 @@
 #import <Foundation/Foundation.h>
 #import "SEGAnalyticsUtils.h"
 
+
 @interface SEGUtils : NSObject
 
-+ (NSData * _Nullable)dataFromPlist:(nonnull id)plist;
-+ (id _Nullable)plistFromData:(NSData * _Nonnull)data;
++ (NSData *_Nullable)dataFromPlist:(nonnull id)plist;
++ (id _Nullable)plistFromData:(NSData *_Nonnull)data;
 
 @end
