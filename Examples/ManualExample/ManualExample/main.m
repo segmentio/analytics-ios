0b615abde1aa6139644244da3f4a6ca16f345a93
tonyxiao <tonyx.ca@gmail.com>
2016-11-28 21:10:55 -0800

    [Blocked] Add example for how to manually include Analytics.framework without any dependency management system (#643)
    * Standalone xcodeproj to build analytics framework
    
    * Sharing Analytics and AnalyticsTests schemes
    
    * Update framework headers visibility. Import SEGAnalytics.h header by default in framework target
    
    * Reverse the order of definitions in SEGAnalytics.h to remove warning
    
    * Further adjusting public vs. private header in Analytics.framework
    
    * Removing Tests from Cocoapods Example project now that they are in the main framework project
    
    * Update makefile and circle.yml
    
    * Adding Carthage build example to demonstrate project working
    
    * Add example for how to manually include Analytics.framework without any dependency management system

---
+++ Examples/ManualExample/ManualExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ManualExample
+//
+//  Created by Tony Xiao on 6/30/16.
+//  Copyright © 2016 Segment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
