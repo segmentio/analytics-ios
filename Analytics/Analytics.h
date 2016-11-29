01f1895bd318189f18eedeb4aa0fe07ddba43f92
Tony Xiao <tonyx.ca@gmail.com>
2016-11-28 20:36:55 -0800

    Further adjusting public vs. private header in Analytics.framework

--- Analytics/Analytics.h
+++ Analytics/Analytics.h
@@ -14,5 +14,5 @@ FOUNDATION_EXPORT double AnalyticsVersionNumber;
 //! Project version string for Analytics.
 FOUNDATION_EXPORT const unsigned char AnalyticsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Analytics/PublicHeader.h>
 #import "SEGAnalytics.h"
+#import "SEGSegmentIntegration.h"
--- AnalyticsTests/AnalyticsTests-Bridging-Header.h
+++ AnalyticsTests/AnalyticsTests-Bridging-Header.h
@@ -2,15 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <Analytics/SEGHTTPClient.h>
-#import <Analytics/SEGCrypto.h>
+#import <Analytics/SEGIntegrationsManager.h>
 #import <Analytics/SEGAES256Crypto.h>
-#import <Analytics/SEGStorage.h>
 #import <Analytics/SEGFileStorage.h>
 #import <Analytics/SEGUserDefaultsStorage.h>
-#import <Analytics/SEGContext.h>
-#import <Analytics/SEGIntegrationsManager.h>
-#import <Analytics/SEGSegmentIntegration.h>
 #import <Analytics/NSData+SEGGZIP.h>
 
 #import "NSData+SEGGUNZIPP.h"
