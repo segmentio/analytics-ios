ff43e95180a89e991bcc3cfb1033a250a0535fc0
Tony Xiao <tonyx.ca@gmail.com>
2016-11-28 20:53:23 -0800

    Add example for how to manually include Analytics.framework without any dependency management system

---
+++ Examples/ManualExample/ManualExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ManualExample
+//
+//  Created by Tony Xiao on 6/30/16.
+//  Copyright © 2016 Segment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
