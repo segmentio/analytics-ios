2c271a0f89bbe8ff8049d122d549ecb86aaefa10
Tony Xiao <tonyx.ca@gmail.com>
2016-11-28 20:17:19 -0800

    Update framework headers visibility. Import SEGAnalytics.h header by default in framework target

--- Analytics/Analytics.h
+++ Analytics/Analytics.h
@@ -15,3 +15,4 @@ FOUNDATION_EXPORT double AnalyticsVersionNumber;
 FOUNDATION_EXPORT const unsigned char AnalyticsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Analytics/PublicHeader.h>
+#import "SEGAnalytics.h"
