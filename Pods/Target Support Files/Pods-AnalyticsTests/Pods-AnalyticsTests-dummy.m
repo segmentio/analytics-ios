3b18825eebf6fda0e3fd917a3c5177db6505283d
Tony Xiao <tonyx.ca@gmail.com>
2017-04-28 19:23:46 -0700

    Removing Pods directory from version control

--- Pods/Target Support Files/Pods-AnalyticsTests/Pods-AnalyticsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AnalyticsTests : NSObject
-@end
-@implementation PodsDummy_Pods_AnalyticsTests
-@end
--- Pods/Target Support Files/SwiftTryCatch/SwiftTryCatch-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SwiftTryCatch.h"
-
-FOUNDATION_EXPORT double SwiftTryCatchVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftTryCatchVersionString[];
-
