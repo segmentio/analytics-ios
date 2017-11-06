1765292d83e7635c74537a6c19bde2f34f5d6481
Prateek Srivastava <f2prateek@gmail.com>
2017-11-06 15:07:30 -0800

    Add Integration Manager tests.
    This required exposing `+ isTrackEvent: enabledForIntegration: inPlan:;`
    
    I also made this and some other relevant methods class methods as they don't rely on class properties.

--- AnalyticsTests/AnalyticsTests-Bridging-Header.h
+++ AnalyticsTests/AnalyticsTests-Bridging-Header.h
@@ -10,6 +10,7 @@
 #import <Analytics/SEGStoreKitTracker.h>
 #import <Analytics/UIViewController+SEGScreen.h>
 #import <Analytics/SEGAnalyticsUtils.h>
+#import <Analytics/SEGIntegrationsManager.h>
 
 #import "NSData+SEGGUNZIPP.h"
 // Temp hack. We should fix the LSNocilla podspec to make this header publicly available
