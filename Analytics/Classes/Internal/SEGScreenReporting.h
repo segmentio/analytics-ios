17706b6b682a59a4dfe3aef9bb602b05a5b96dc3
David Whetstone <david@humblehacker.com>
2018-07-25 11:16:59 -0700

    Make seg_topViewController handle tab and custom container VCs

--- Analytics/Classes/Internal/SEGScreenReporting.h
+++ Analytics/Classes/Internal/SEGScreenReporting.h
@@ -1,4 +1,4 @@
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "SEGSerializableValue.h"
 
 /** Implement this protocol to add properties to automatic screen reporting
@@ -9,6 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 @protocol SEGScreenReporting
 @optional @property (readonly, nullable) NSString * seg_screenName;
 @optional @property (readonly, nullable) SERIALIZABLE_DICT seg_screenProperties;
+@optional @property (readonly, nullable) UIViewController *seg_mainViewController;
 @end
 
 NS_ASSUME_NONNULL_END
