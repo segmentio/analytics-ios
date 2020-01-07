fe04eaa780fdd80f6736a25c1fd4d8106985f67a
Brandon Sneed <brandon.sneed@segment.com>
2020-01-07 15:59:36 -0800

    Remove extraneous NSNull handling causing tests to fail.

---
+++ Analytics/Classes/Internal/SEGMacros.h
@@ -0,0 +1,13 @@
+//
+//  SEGMacros.h
+//  Analytics
+//
+//  Created by Brandon Sneed on 12/20/19.
+//  Copyright © 2019 Segment. All rights reserved.
+//
+
+#ifndef SEGMacros_h
+#define SEGMacros_h
+
+
+#endif /* SEGMacros_h */
