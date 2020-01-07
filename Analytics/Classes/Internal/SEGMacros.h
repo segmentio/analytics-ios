5d2fbd44394308c6029a0c7e5b75fea148612e8e
Brandon Sneed <brandon.sneed@segment.com>
2020-01-07 15:59:59 -0800

    Added weakify/strongify macros.

--- Analytics/Classes/Internal/SEGMacros.h
+++ Analytics/Classes/Internal/SEGMacros.h
@@ -9,5 +9,14 @@
 #ifndef SEGMacros_h
 #define SEGMacros_h
 
+#define __deprecated__(s) __attribute__((deprecated(s)))
+
+#define weakify(var) __weak typeof(var) __weak_##var = var;
+
+#define strongify(var) \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Wshadow\"") \
+__strong typeof(var) var = __weak_##var; \
+_Pragma("clang diagnostic pop")
 
 #endif /* SEGMacros_h */
