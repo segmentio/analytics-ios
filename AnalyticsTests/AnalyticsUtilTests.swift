06f5f4cfc8be060f08fa4d41155d9bc930e5b9ed
Brandon Sneed <brandon.sneed@segment.com>
2020-04-14 14:47:41 -0700

    Added nanosecond time test.

--- AnalyticsTests/AnalyticsUtilTests.swift
+++ AnalyticsTests/AnalyticsUtilTests.swift
@@ -17,7 +17,7 @@ class AnalyticsUtilTests: QuickSpec {
     it("format NSDate objects to RFC 3339 complaint string") {
       let date = Date(timeIntervalSince1970: 0)
       let formattedString = iso8601FormattedString(date)
-      expect(formattedString) == "1970-01-01T00:00:00.000000Z"
+      expect(formattedString) == "1970-01-01T00:00:00.000Z"
 
       var components = DateComponents()
       components.year = 1992
@@ -31,7 +31,27 @@ class AnalyticsUtilTests: QuickSpec {
       calendar.timeZone = TimeZone(secondsFromGMT: -4 * 60 * 60)!
       let date2 = calendar.date(from: components)!
       let formattedString2 = iso8601FormattedString(date2)
-      expect(formattedString2) == "1992-08-06T11:32:04.335000Z"
+      expect(formattedString2) == "1992-08-06T11:32:04.335Z"
+    }
+
+    it("format NSDate objects to RFC 3339 complaint string w/ nanoseconds") {
+      let date = Date(timeIntervalSince1970: 0)
+      let formattedString = iso8601NanoFormattedString(date)
+      expect(formattedString) == "1970-01-01T00:00:00.000000000Z"
+
+      var components = DateComponents()
+      components.year = 1992
+      components.month = 8
+      components.day = 6
+      components.hour = 7
+      components.minute = 32
+      components.second = 4
+      components.nanosecond = 335000008
+      let calendar = NSCalendar(calendarIdentifier: .gregorian)!
+      calendar.timeZone = TimeZone(secondsFromGMT: -4 * 60 * 60)!
+      let date2 = calendar.date(from: components)!
+      let formattedString2 = iso8601NanoFormattedString(date2)
+      expect(formattedString2) == "1992-08-06T11:32:04.335000008Z"
     }
 
     describe("trimQueue", {
