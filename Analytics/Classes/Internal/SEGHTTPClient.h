8158687dd3b8626bce74a70929361808b362e5f5
Cristian Lupu <cristian.lupu@ellation.com>
2020-04-16 00:50:09 +0300

    Implement maximum batch request size (#874)
    * Implement maximum request size
    
    * Add test when batch exceeds the size
    
    * Execute check before gzip
    
    * Remove stub request

--- Analytics/Classes/Internal/SEGHTTPClient.h
+++ Analytics/Classes/Internal/SEGHTTPClient.h
@@ -33,7 +33,7 @@ NS_ASSUME_NONNULL_BEGIN
  * NOTE: You need to re-dispatch within the completionHandler onto a desired queue to avoid threading issues.
  * Completion handlers are called on a dispatch queue internal to SEGHTTPClient. 
  */
-- (NSURLSessionUploadTask *)upload:(JSON_DICT)batch forWriteKey:(NSString *)writeKey completionHandler:(void (^)(BOOL retry))completionHandler;
+- (nullable NSURLSessionUploadTask *)upload:(JSON_DICT)batch forWriteKey:(NSString *)writeKey completionHandler:(void (^)(BOOL retry))completionHandler;
 
 - (NSURLSessionDataTask *)settingsForWriteKey:(NSString *)writeKey completionHandler:(void (^)(BOOL success, JSON_DICT _Nullable settings))completionHandler;
 
