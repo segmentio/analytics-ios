86fe29f2523ee5cf79916c18fd76471d6418990a
Brandon Sneed <brandon@redf.net>
2020-05-20 15:58:38 -0700

    Add ability to set default settings is segment.com can't be reached. (#888)
    * Added integration middleware support.
    
    * Fixed warnings; Updated project to recommended settings.
    
    * only signal the runner if there’s actually middleware to be processed.
    
    * Added & Updated tests.
    
    * Added experimental raw filter block.
    
    * Removed unnecessary logs.
    
    * Added logic to allow tests to function.
    
    * Allow for user-supplied settings.
    
    * Make sure segment.io is still present in the integrations list.
    
    * Added ability to set default settings if segment.com can’t be reached.
    
    * Removed test code.
    
    Co-authored-by: Brandon Sneed <brandon.sneed@segment.com>

--- Analytics/Classes/SEGAnalyticsConfiguration.h
+++ Analytics/Classes/SEGAnalyticsConfiguration.h
@@ -128,6 +128,13 @@ typedef NSMutableURLRequest *_Nonnull (^SEGRequestFactory)(NSURL *_Nonnull);
  */
 @property (nonatomic, strong, nullable) id<SEGCrypto> crypto;
 
+
+/**
+ * Set the default settings to use if Segment.com cannot be reached.  JSON->NSDictionary conversion is handled for you.
+ * An example configuration can be found here, using your write key:  https://cdn-settings.segment.com/v1/projects/YOUR_WRITE_KEY/settings
+ */
+@property (nonatomic, strong, nullable) NSDictionary *defaultSettings;
+
 /**
  * Set custom middlewares. Will be run before all integrations.
  *  This property is deprecated in favor of the `sourceMiddleware` property.
