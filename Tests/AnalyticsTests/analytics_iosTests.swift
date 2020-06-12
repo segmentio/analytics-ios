69b6781f8c35b7f19fa869e9fa60e4f099d78595
Cody Garvin <emig647@gmail.com>
2020-06-12 12:07:45 -0700

    Added first pass on SPM

---
+++ Tests/AnalyticsTests/analytics_iosTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Analytics
+
+final class analytics_iosTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssert(true, "Failed Test Example")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import analytics_iosTests
+
+var tests = [XCTestCaseEntry]()
+tests += analytics_iosTests.allTests()
+XCTMain(tests)
