6e3a8ed6dcb5483d58eb550c7129ad247379e38a
Cody Garvin <emig647@gmail.com>
2020-06-15 11:03:54 -0700

    Removed platform requirements

--- Package.swift
+++ Package.swift
@@ -5,11 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Analytics",
-    platforms: [
-        // The specific platforms to support
-        .iOS(.v10),
-        .tvOS(.v9)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
