c5bcbd553537e58fd5af303a62bd67fd67f53966
Cody Garvin <emig647@gmail.com>
2020-06-23 11:35:23 -0700

    LIBMOBILE-77: Removed Quick and Nimble, updated unit tests (#909)

--- AnalyticsTests/AnalyticsTests.swift
+++ AnalyticsTests/AnalyticsTests.swift
@@ -7,227 +7,212 @@
 //
 
 
-import Quick
-import Nimble
 import Analytics
+import XCTest
 
-class AnalyticsTests: QuickSpec {
-  override func spec() {
+class AnalyticsTests: XCTestCase {
+    
     let config = AnalyticsConfiguration(writeKey: "QUI5ydwIGeFFTa1IvCBUhxL9PyW5B0jE")
     let cachedSettings = [
-      "integrations": [
-        "Segment.io": ["apiKey": "QUI5ydwIGeFFTa1IvCBUhxL9PyW5B0jE"]
-      ],
-      "plan": ["track": [:]],
-    ] as NSDictionary
+        "integrations": [
+            "Segment.io": ["apiKey": "QUI5ydwIGeFFTa1IvCBUhxL9PyW5B0jE"]
+        ],
+        "plan": ["track": [:]],
+        ] as NSDictionary
     var analytics: Analytics!
     var testMiddleware: TestMiddleware!
     var testApplication: TestApplication!
-
-    beforeEach {
-      testMiddleware = TestMiddleware()
-      config.sourceMiddleware = [testMiddleware]
-      testApplication = TestApplication()
-      config.application = testApplication
-      config.trackApplicationLifecycleEvents = true
-
-      UserDefaults.standard.set("test SEGQueue should be removed", forKey: "SEGQueue")
-      expect(UserDefaults.standard.string(forKey: "SEGQueue")).toNot(beNil())
-      
-      analytics = Analytics(configuration: config)
-      analytics.test_integrationsManager()?.test_setCachedSettings(settings: cachedSettings)
-    }
-
-    afterEach {
-      analytics.reset()
-    }
-
-    it("initialized correctly") {
-      expect(analytics.configuration.flushAt) == 20
-      expect(analytics.configuration.flushInterval) == 30
-      expect(analytics.configuration.maxQueueSize) == 1000
-      expect(analytics.configuration.writeKey) == "QUI5ydwIGeFFTa1IvCBUhxL9PyW5B0jE"
-      expect(analytics.configuration.shouldUseLocationServices) == false
-      expect(analytics.configuration.enableAdvertisingTracking) == true
-      expect(analytics.configuration.shouldUseBluetooth) == false
-      expect(analytics.configuration.httpSessionDelegate).to(beNil())
-      expect(analytics.getAnonymousId()).toNot(beNil())
+    
+    override func setUp() {
+        super.setUp()
+        testMiddleware = TestMiddleware()
+        config.sourceMiddleware = [testMiddleware]
+        testApplication = TestApplication()
+        config.application = testApplication
+        config.trackApplicationLifecycleEvents = true
+        
+        UserDefaults.standard.set("test SEGQueue should be removed", forKey: "SEGQueue")
+        XCTAssertNotNil(UserDefaults.standard.string(forKey: "SEGQueue"))
+        
+        analytics = Analytics(configuration: config)
+        analytics.test_integrationsManager()?.test_setCachedSettings(settings: cachedSettings)
     }
     
-    it("clears SEGQueue from UserDefaults after initialized") {
-      expect(UserDefaults.standard.string(forKey: "SEGQueue")).toEventually(beNil())
+    override func tearDown() {
+        super.tearDown()
+        analytics.reset()
+    }
+    
+    func testInitializedCorrectly() {
+        XCTAssertEqual(analytics.configuration.flushAt, 20)
+        XCTAssertEqual(analytics.configuration.flushInterval, 30)
+        XCTAssertEqual(analytics.configuration.maxQueueSize, 1000)
+        XCTAssertEqual(analytics.configuration.writeKey, "QUI5ydwIGeFFTa1IvCBUhxL9PyW5B0jE")
+        XCTAssertEqual(analytics.configuration.shouldUseLocationServices, false)
+        XCTAssertEqual(analytics.configuration.enableAdvertisingTracking, true)
+        XCTAssertEqual(analytics.configuration.shouldUseBluetooth,  false)
+        XCTAssertNil(analytics.configuration.httpSessionDelegate)
+        XCTAssertNotNil(analytics.getAnonymousId())
+    }
+    
+    func testClearsSEGQueueFromUserDefaults() {
+        expectUntil(2.0, expression: UserDefaults.standard.string(forKey: "SEGQueue") == nil)
     }
     
     /* TODO: Fix me when the Context object isn't so wild.
-    it("collects IDFA") {
-      testMiddleware.swallowEvent = true
-      analytics.configuration.enableAdvertisingTracking = true
-      analytics.configuration.adSupportBlock = { () -> String in
-        return "1234AdsNoMore!"
-      }
-
-      analytics.track("test");
-      
-      let event = testMiddleware.lastContext?.payload as? SEGTrackPayload
-      expect(event?.properties?["url"] as? String) == "myapp://auth?token=((redacted/my-auth))&other=stuff"
-    }*/
+     func testCollectsIDFA() {
+     testMiddleware.swallowEvent = true
+     analytics.configuration.enableAdvertisingTracking = true
+     analytics.configuration.adSupportBlock = { () -> String in
+     return "1234AdsNoMore!"
+     }
+     
+     analytics.track("test");
+     
+     let event = testMiddleware.lastContext?.payload as? TrackPayload
+     XCTAssertEqual(event?.properties?["url"] as? String, "myapp://auth?token=((redacted/my-auth))&other=stuff")
+     }*/
     
-    it("persists anonymousId") {
-      let analytics2 = Analytics(configuration: config)
-      expect(analytics.getAnonymousId()) == analytics2.getAnonymousId()
-    }
-
-    it("persists userId") {
-      analytics.identify("testUserId1")
-
-      let analytics2 = Analytics(configuration: config)
-      analytics2.test_integrationsManager()?.test_setCachedSettings(settings: cachedSettings)
-
-      expect(analytics.test_integrationsManager()?.test_segmentIntegration()?.test_userId()) == "testUserId1"
-      expect(analytics2.test_integrationsManager()?.test_segmentIntegration()?.test_userId()) == "testUserId1"
-    }
-
-    /* This test is pretty flaky.
-    it("continues user activity") {
-      let activity = NSUserActivity(activityType: NSUserActivityTypeBrowsingWeb)
-      activity.webpageURL = URL(string: "http://www.segment.com")
-      analytics.continue(activity)
-      let referrer = analytics.test_integrationsManager()?.test_segmentIntegration()?.test_referrer()
-      expect(referrer?["url"] as? String).toEventually(equal("http://www.segment.com"))
-    }
-    */
-
-    it("clears user data") {
-      analytics.identify("testUserId1", traits: [ "Test trait key" : "Test trait value"])
-      analytics.reset()
-      expect(analytics.test_integrationsManager()?.test_segmentIntegration()?.test_userId()).toEventually(beNil())
-      expect(analytics.test_integrationsManager()?.test_segmentIntegration()?.test_traits()?.count).toEventually(equal(0))
-    }
-
-    it("fires Application Opened for UIApplicationDidFinishLaunching") {
-      testMiddleware.swallowEvent = true
-      NotificationCenter.default.post(name: .UIApplicationDidFinishLaunching, object: testApplication, userInfo: [
-        UIApplicationLaunchOptionsKey.sourceApplication: "testApp",
-        UIApplicationLaunchOptionsKey.url: "test://test",
-      ])
-      let event = testMiddleware.lastContext?.payload as? TrackPayload
-      expect(event?.event) == "Application Opened"
-      expect(event?.properties?["from_background"] as? Bool) == false
-      expect(event?.properties?["referring_application"] as? String) == "testApp"
-      expect(event?.properties?["url"] as? String) == "test://test"
-    }
-
-    it("fires Application Opened during UIApplicationWillEnterForeground") {
-      testMiddleware.swallowEvent = true
-      NotificationCenter.default.post(name: .UIApplicationWillEnterForeground, object: testApplication)
-      let event = testMiddleware.lastContext?.payload as? TrackPayload
-      expect(event?.event) == "Application Opened"
-      expect(event?.properties?["from_background"] as? Bool) == true
+    func testPersistsAnonymousId() {
+        let analytics2 = Analytics(configuration: config)
+        XCTAssertEqual(analytics.getAnonymousId(), analytics2.getAnonymousId())
     }
     
-    it("fires Application Backgrounded during UIApplicationDidEnterBackground") {
-      testMiddleware.swallowEvent = true
-      NotificationCenter.default.post(name: .UIApplicationDidEnterBackground, object: testApplication)
-      let event = testMiddleware.lastContext?.payload as? TrackPayload
-      expect(event?.event) == "Application Backgrounded"
-    }
-
-    it("flushes when UIApplicationDidEnterBackgroundNotification is fired") {
-      analytics.track("test")
-      NotificationCenter.default.post(name: .UIApplicationDidEnterBackground, object: testApplication)
-      expect(testApplication.backgroundTasks.count).toEventually(equal(1))
-      expect(testApplication.backgroundTasks[0].isEnded).toEventually(beFalse())
-    }
-    
-    it("respects maxQueueSize") {
-      let max = 72
-      config.maxQueueSize = UInt(max)
-
-      for i in 1...max * 2 {
-        analytics.track("test #\(i)")
-      }
-
-      let integration = analytics.test_integrationsManager()?.test_segmentIntegration()
-      expect(integration).notTo(beNil())
-      
-      analytics.flush()
-      waitUntil(timeout: 60) {done in
-        let queue = DispatchQueue(label: "test")
+    func testPersistsUserId() {
+        analytics.identify("testUserId1")
         
-        queue.async {
-          while(integration?.test_queue()?.count != max) {
-            sleep(1)
-          }
-
-          done()
+        let analytics2 = Analytics(configuration: config)
+        analytics2.test_integrationsManager()?.test_setCachedSettings(settings: cachedSettings)
+        
+        XCTAssertEqual(analytics.test_integrationsManager()?.test_segmentIntegration()?.test_userId(), "testUserId1")
+        XCTAssertEqual(analytics2.test_integrationsManager()?.test_segmentIntegration()?.test_userId(), "testUserId1")
+    }
+    
+    func testClearsUserData() {
+        analytics.identify("testUserId1", traits: [ "Test trait key" : "Test trait value"])
+        analytics.reset()
+        
+        expectUntil(2.0, expression: self.analytics.test_integrationsManager()?.test_segmentIntegration()?.test_userId() == nil)
+        
+        expectUntil(2.0, expression: self.analytics.test_integrationsManager()?.test_segmentIntegration()?.test_traits()?.count == 0)
+    }
+    
+    func testFiresApplicationOpenedForAppLaunchingEvent() {
+        testMiddleware.swallowEvent = true
+        NotificationCenter.default.post(name: .UIApplicationDidFinishLaunching, object: testApplication, userInfo: [
+            UIApplicationLaunchOptionsKey.sourceApplication: "testApp",
+            UIApplicationLaunchOptionsKey.url: "test://test",
+        ])
+        let event = testMiddleware.lastContext?.payload as? TrackPayload
+        XCTAssertEqual(event?.event, "Application Opened")
+        XCTAssertEqual(event?.properties?["from_background"] as? Bool, false)
+        XCTAssertEqual(event?.properties?["referring_application"] as? String, "testApp")
+        XCTAssertEqual(event?.properties?["url"] as? String, "test://test")
+    }
+    
+    func testFiresApplicationEnterForeground() {
+        testMiddleware.swallowEvent = true
+        NotificationCenter.default.post(name: .UIApplicationWillEnterForeground, object: testApplication)
+        let event = testMiddleware.lastContext?.payload as? TrackPayload
+        XCTAssertEqual(event?.event, "Application Opened")
+        XCTAssertEqual(event?.properties?["from_background"] as? Bool, true)
+    }
+    
+    func testFiresApplicationDuringEnterBackground() {
+        testMiddleware.swallowEvent = true
+        NotificationCenter.default.post(name: .UIApplicationDidEnterBackground, object: testApplication)
+        let event = testMiddleware.lastContext?.payload as? TrackPayload
+        XCTAssertEqual(event?.event, "Application Backgrounded")
+    }
+    
+    func testFlushesWhenApplicationBackgroundIsFired() {
+        analytics.track("test")
+        NotificationCenter.default.post(name: .UIApplicationDidEnterBackground, object: testApplication)
+        
+        expectUntil(2.0, expression: self.testApplication.backgroundTasks.count == 1)
+        expectUntil(2.0, expression: self.testApplication.backgroundTasks[0].isEnded == false)
+    }
+    
+    func testRespectsMaxQueueSize() {
+        let max = 72
+        config.maxQueueSize = UInt(max)
+        
+        for i in 1...max * 2 {
+            analytics.track("test #\(i)")
         }
-      }
-    }
-
-    it("protocol conformance should not interfere with UIApplication interface") {
-      // In Xcode8/iOS10, UIApplication.h typedefs UIBackgroundTaskIdentifier as NSUInteger,
-      // whereas Swift has UIBackgroundTaskIdentifier typealiaed to Int.
-      // This is likely due to a custom Swift mapping for UIApplication which got out of sync.
-      // If we extract the exact UIApplication method names in SEGApplicationProtocol,
-      // it will cause a type mismatch between the return value from beginBackgroundTask
-      // and the argument for endBackgroundTask.
-      // This would impact all code in a project that imports the Segment framework.
-      // Note that this doesn't appear to be an issue any longer in Xcode9b3.
-      let task = UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
-      UIApplication.shared.endBackgroundTask(task)
+        
+        let integration = analytics.test_integrationsManager()?.test_segmentIntegration()
+        XCTAssertNotNil(integration)
+        
+        analytics.flush()
+        let currentTime = Date()
+        while(integration?.test_queue()?.count != max && currentTime < currentTime + 60) {
+            sleep(1)
+        }
     }
     
-    it("flushes using flushTimer") {
-      let integration = analytics.test_integrationsManager()?.test_segmentIntegration()
-
-      analytics.track("test")
-
-      expect(integration?.test_flushTimer()).toEventuallyNot(beNil())
-      expect(integration?.test_batchRequest()).to(beNil())
-
-      integration?.test_flushTimer()?.fire()
-      
-      expect(integration?.test_batchRequest()).toEventuallyNot(beNil())
-    }
-
-    it("respects flushInterval") {
-      let timer = analytics
-        .test_integrationsManager()?
-        .test_segmentIntegration()?
-        .test_flushTimer()
-      
-      expect(timer).toNot(beNil())
-      expect(timer?.timeInterval) == config.flushInterval
+    func testProtocolConformanceShouldNotInterfere() {
+        // In Xcode8/iOS10, UIApplication.h typedefs UIBackgroundTaskIdentifier as NSUInteger,
+        // whereas Swift has UIBackgroundTaskIdentifier typealiaed to Int.
+        // This is likely due to a custom Swift mapping for UIApplication which got out of sync.
+        // If we extract the exact UIApplication method names in SEGApplicationProtocol,
+        // it will cause a type mismatch between the return value from beginBackgroundTask
+        // and the argument for endBackgroundTask.
+        // This would impact all code in a project that imports the Segment framework.
+        // Note that this doesn't appear to be an issue any longer in Xcode9b3.
+        let task = UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
+        UIApplication.shared.endBackgroundTask(task)
     }
     
-    it("redacts sensible URLs from deep links tracking") {
-      testMiddleware.swallowEvent = true
-      analytics.configuration.trackDeepLinks = true
-      analytics.open(URL(string: "fb123456789://authorize#access_token=hastoberedacted")!, options: [:])
-      
-      
-      let event = testMiddleware.lastContext?.payload as? TrackPayload
-      expect(event?.event) == "Deep Link Opened"
-      expect(event?.properties?["url"] as? String) == "fb123456789://authorize#access_token=((redacted/fb-auth-token))"
-    }
-
-    it("redacts sensible URLs from deep links tracking using custom filters") {
-      testMiddleware.swallowEvent = true
-      analytics.configuration.payloadFilters["(myapp://auth\\?token=)([^&]+)"] = "$1((redacted/my-auth))"
-      analytics.configuration.trackDeepLinks = true
-      analytics.open(URL(string: "myapp://auth?token=hastoberedacted&other=stuff")!, options: [:])
-      
-      
-      let event = testMiddleware.lastContext?.payload as? TrackPayload
-      expect(event?.event) == "Deep Link Opened"
-      expect(event?.properties?["url"] as? String) == "myapp://auth?token=((redacted/my-auth))&other=stuff"
+    func testFlushesUsingFlushTimer() {
+        let integration = analytics.test_integrationsManager()?.test_segmentIntegration()
+        
+        analytics.track("test")
+        
+        expectUntil(2.0, expression: integration?.test_flushTimer() != nil)
+        XCTAssertNil(integration?.test_batchRequest())
+        
+        integration?.test_flushTimer()?.fire()
+        expectUntil(2.0, expression: integration?.test_batchRequest() != nil)
     }
     
-    it("defaults SEGQueue to an empty array when missing from file storage") {
-      let integration = analytics.test_integrationsManager()?.test_segmentIntegration()
-      expect(integration).notTo(beNil())
-      integration?.test_fileStorage()?.resetAll()
-      expect(integration?.test_queue()).to(beEmpty())
+    func testRespectsFlushIntervale() {
+        let timer = analytics
+            .test_integrationsManager()?
+            .test_segmentIntegration()?
+            .test_flushTimer()
+        
+        XCTAssertNotNil(timer)
+        XCTAssertEqual(timer?.timeInterval, config.flushInterval)
     }
-  }
+    
+    func testRedactsSensibleURLsFromDeepLinksTracking() {
+        testMiddleware.swallowEvent = true
+        analytics.configuration.trackDeepLinks = true
+        analytics.open(URL(string: "fb123456789://authorize#access_token=hastoberedacted")!, options: [:])
+        
+        
+        let event = testMiddleware.lastContext?.payload as? TrackPayload
+        XCTAssertEqual(event?.event, "Deep Link Opened")
+        XCTAssertEqual(event?.properties?["url"] as? String, "fb123456789://authorize#access_token=((redacted/fb-auth-token))")
+    }
+    
+    func testRedactsSensibleURLsFromDeepLinksWithFilters() {
+        testMiddleware.swallowEvent = true
+        analytics.configuration.payloadFilters["(myapp://auth\\?token=)([^&]+)"] = "$1((redacted/my-auth))"
+        analytics.configuration.trackDeepLinks = true
+        analytics.open(URL(string: "myapp://auth?token=hastoberedacted&other=stuff")!, options: [:])
+        
+        
+        let event = testMiddleware.lastContext?.payload as? TrackPayload
+        XCTAssertEqual(event?.event, "Deep Link Opened")
+        XCTAssertEqual(event?.properties?["url"] as? String, "myapp://auth?token=((redacted/my-auth))&other=stuff")
+    }
+    
+    func testDefaultsSEGQueueToEmptyArray() {
+        let integration = analytics.test_integrationsManager()?.test_segmentIntegration()
+        XCTAssertNotNil(integration)
+        integration?.test_fileStorage()?.resetAll()
+        XCTAssert(integration?.test_queue()?.isEmpty ?? false)
+    }
 }
