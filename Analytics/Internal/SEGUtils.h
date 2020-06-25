4e3df7a9a269c729cd575ad3b3376b4242504f57
Brandon Sneed <brandon@redf.net>
2020-06-25 14:12:18 -0700

    Separate public utils from private utils appropriately (#913)
    * Separated public utils from private utils
    
    * Made SEGAnalyticsUtils public.
    
    * Fixed header includes.
    
    Co-authored-by: Brandon Sneed <brandon.sneed@segment.com>

--- Analytics/Internal/SEGUtils.h
+++ Analytics/Internal/SEGUtils.h
@@ -5,6 +5,7 @@
 
 #import <Foundation/Foundation.h>
 #import "SEGAnalyticsUtils.h"
+#import "SEGSerializableValue.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -28,5 +29,44 @@ NSString *getDeviceModel(void);
 BOOL getAdTrackingEnabled(SEGAnalyticsConfiguration *configuration);
 NSDictionary *getStaticContext(SEGAnalyticsConfiguration *configuration, NSString * _Nullable deviceToken);
 NSDictionary *getLiveContext(SEGReachability *reachability, NSDictionary * _Nullable referrer, NSDictionary * _Nullable traits);
+
+NSString *GenerateUUIDString(void);
+
+// Date Utils
+NSString *iso8601FormattedString(NSDate *date);
+NSString *iso8601NanoFormattedString(NSDate *date);
+
+void trimQueue(NSMutableArray *array, NSUInteger size);
+
+// Async Utils
+dispatch_queue_t seg_dispatch_queue_create_specific(const char *label,
+                                                    dispatch_queue_attr_t _Nullable attr);
+BOOL seg_dispatch_is_on_specific_queue(dispatch_queue_t queue);
+void seg_dispatch_specific(dispatch_queue_t queue, dispatch_block_t block,
+                           BOOL waitForCompletion);
+void seg_dispatch_specific_async(dispatch_queue_t queue,
+                                 dispatch_block_t block);
+void seg_dispatch_specific_sync(dispatch_queue_t queue, dispatch_block_t block);
+
+// JSON Utils
+
+JSON_DICT SEGCoerceDictionary(NSDictionary *_Nullable dict);
+
+NSString *_Nullable SEGIDFA(void);
+
+NSString *SEGEventNameForScreenTitle(NSString *title);
+
+// Deep copy and check NSCoding conformance
+@protocol SEGSerializableDeepCopy <NSObject>
+-(id _Nullable) serializableMutableDeepCopy;
+-(id _Nullable) serializableDeepCopy;
+@end
+
+@interface NSDictionary(SerializableDeepCopy) <SEGSerializableDeepCopy>
+@end
+
+@interface NSArray(SerializableDeepCopy) <SEGSerializableDeepCopy>
+@end
+
 
 NS_ASSUME_NONNULL_END
