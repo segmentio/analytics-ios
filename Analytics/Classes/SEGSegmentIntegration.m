90d738b5e5e1907ab6c6adc3548f260beff43910
Brandon Sneed <brandon.sneed@segment.com>
2020-07-13 11:30:02 -0700

    Fixed issue with userId not being cleared properly in a reset.

--- Analytics/Classes/SEGSegmentIntegration.m
+++ Analytics/Classes/SEGSegmentIntegration.m
@@ -62,14 +62,14 @@ NSString *const kSEGTraitsFilename = @"segmentio.traits.plist";
         self.fileStorage = fileStorage;
         self.userDefaultsStorage = userDefaultsStorage;
         self.apiURL = [SEGMENT_API_BASE URLByAppendingPathComponent:@"import"];
-        self.userId = [self getUserId];
         self.reachability = [SEGReachability reachabilityWithHostname:@"google.com"];
         [self.reachability startNotifier];
         self.serialQueue = seg_dispatch_queue_create_specific("io.segment.analytics.segmentio", DISPATCH_QUEUE_SERIAL);
         self.backgroundTaskQueue = seg_dispatch_queue_create_specific("io.segment.analytics.backgroundTask", DISPATCH_QUEUE_SERIAL);
         self.flushTaskID = UIBackgroundTaskInvalid;
         
-        // load traits from disk.
+        // load traits & user from disk.
+        [self loadUserId];
         [self loadTraits];
 
         [self dispatchBackground:^{
@@ -150,10 +150,14 @@ NSString *const kSEGTraitsFilename = @"segmentio.traits.plist";
     return [NSString stringWithFormat:@"<%p:%@, %@>", self, self.class, self.configuration.writeKey];
 }
 
-- (void)saveUserId:(NSString *)userId
+- (NSString *)userId
+{
+    return [SEGState sharedInstance].userInfo.userId;
+}
+
+- (void)setUserId:(NSString *)userId
 {
     [self dispatchBackground:^{
-        self.userId = userId;
         [SEGState sharedInstance].userInfo.userId = userId;
 #if TARGET_OS_TV
         [self.userDefaultsStorage setString:userId forKey:SEGUserIdKey];
@@ -170,11 +174,6 @@ NSString *const kSEGTraitsFilename = @"segmentio.traits.plist";
 
 - (void)setTraits:(NSDictionary *)traits
 {
-    [self saveTraits:traits];
-}
-
-- (void)saveTraits:(NSDictionary *)traits
-{
     [self dispatchBackground:^{
         [SEGState sharedInstance].userInfo.traits = traits;
 #if TARGET_OS_TV
@@ -190,8 +189,8 @@ NSString *const kSEGTraitsFilename = @"segmentio.traits.plist";
 - (void)identify:(SEGIdentifyPayload *)payload
 {
     [self dispatchBackground:^{
-        [self saveUserId:payload.userId];
-        [self saveTraits:payload.traits];
+        self.userId = payload.userId;
+        self.traits = payload.traits;
     }];
 
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
@@ -447,7 +446,7 @@ NSString *const kSEGTraitsFilename = @"segmentio.traits.plist";
     return 100;
 }
 
-- (NSString *)getUserId
+- (void)loadUserId
 {
     NSString *result = nil;
 #if TARGET_OS_TV
@@ -456,7 +455,6 @@ NSString *const kSEGTraitsFilename = @"segmentio.traits.plist";
     result = [self.fileStorage stringForKey:kSEGUserIdFilename];
 #endif
     [SEGState sharedInstance].userInfo.userId = result;
-    return result;
 }
 
 - (void)persistQueue
